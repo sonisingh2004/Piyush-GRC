fb6ab793978c46ea121b94ae962bbc11f2cbc91d
soni <sonisingh5093@gmail.com>
2026-02-25 16:59:40 +0530

    fix font issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,9 +32,7 @@ export default function RootLayout({
           rel="stylesheet" 
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Navbar/>
         {children}
         <Footer/>
